70254a5581546bbcb2ccc4b5a9d8e780e7d91e6b
mantu kumar choudhury <giet.11eee063@gmail.com>
2017-07-15 21:47:19 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { EmployeeserviceService } from './employeeservice.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<app-employeedetails></app-employeedetails> <app-employeelist></app-employeelist>',
+  styleUrls: ['./app.component.css'],
+  providers:[EmployeeserviceService]
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { EmployeedetailsComponent} from './employeedetails/employeedetails.component';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeedetailsComponent,
+    EmployeelistComponent,
+    
   ],
   imports: [
     BrowserModule
