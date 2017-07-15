fc953c4ccd193dc93d6db03f6e1963c18724404b
mantugit <mantu.thedon123@gmail.com>
2017-07-15 22:18:37 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-employeedetails></app-employeedetails> <app-employeelist></app-employeelist>',
+  template: '<h1>Root app</h1> <app-employeedetails></app-employeedetails> <app-employeelist></app-employeelist>',
   styleUrls: ['./app.component.css'],
   providers:[EmployeeserviceService]
 })
